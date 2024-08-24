366ba7b2c44b94cfbb81b49e6630b6eebd3254d8
sonercelix <sonercelix@hotmail.com>
2024-08-25 01:30:53 +0300

    hassas verilerin saklanması ile ilgili 3 yontem orneklendi

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,27 @@ namespace dotnet_secret_management
     {
         static void Main(string[] args)
         {
+            string connString = ConfigurationManager.ConnectionStrings["PersonnelDB"].ToString();
+            string apiKey =  ConfigurationManager.AppSettings["PersonelServiceApiKey"];
+            string path = ConfigurationManager.AppSettings["path"];
+
+
+            //2.yöntem
+            string testApiKey = Environment.GetEnvironmentVariable("TEST_API_KEY");
+
+            //3.yöntem.
+            GetApiKeyFromJsonFile();
+
+            Console.ReadKey();
+        }
+
+        private static void GetApiKeyFromJsonFile()
+        {
+            var secretsFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "secrets.json");
+            var secretsJson = File.ReadAllText(secretsFilePath);
+            var secrets = JObject.Parse(secretsJson);
+
+            string apiKey = secrets["ApiKey"].ToString();
         }
     }
 }
